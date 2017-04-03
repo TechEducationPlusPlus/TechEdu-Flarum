9a616bb941ae06c0727589e6051b93c56b99e0bb
alex <alex@box.techedu.cf>
2017-04-03 09:07:14 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => 'tts2002',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'flarum_',
+    'strict' => false,
+  ),
+  'url' => 'https://chat.techedu.cf',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
